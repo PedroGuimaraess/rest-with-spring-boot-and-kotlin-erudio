37d56b5350ec496d9724e36fa6a674ca57e22b48
PedroNewUser <phpguimaraes2005@gmail.com>
2025-02-17 21:50:11 -0300

    Add sum function

--- FirstStepsInKotlinWithSpringBoot/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/GreetingController.kt
+++ FirstStepsInKotlinWithSpringBoot/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/GreetingController.kt
@@ -1,5 +1,6 @@
 package br.com.erudio
 
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -17,5 +18,10 @@ class GreetingController {
         )
     }
 
-
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    fun sum(@PathVariable(value="numberOne") numberOne: String?,
+            @PathVariable(value="numberTwo") numberTwo: String?
+    ): Double{
+        return 1.0
+    }
 }
